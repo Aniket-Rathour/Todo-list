20d37585d948725fca06c672b2e9269fc114fb74
PANEER <aniketrathour9911@gmail.com>
2025-08-08 00:07:07 +0530

    bit of ui

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -7,18 +7,24 @@ export function CreateTodo({setUpdate, update}){
     //const [compleated , setCompleated] = useState("")
     const [selectedOption, setSelectedOption] = useState("false");
     return <div>
-        <input type="text" placeholder="title" onChange={function(e){
+
+        <input  style={{padding: "8px",fontSize: "16px",border: "2px solid #ccc",borderRadius: "7px",outline: "none",width: "20%",}} 
+        type="text" placeholder="title" onChange={function(e){
             setTitle(e.target.value);
         }}></input><div></div>
-        <input type="text" placeholder="description" onChange={function(e){
+
+        <input style={{padding: "8px",fontSize: "16px",border: "2px solid #ccc",borderRadius: "7px",outline: "none",width: "20%",}} 
+         type="text" placeholder="description" onChange={function(e){
             setDescription(e.target.value);
         }}></input><div></div>
 
-        <select id="select" value={selectedOption.toString()} onChange={(e) => setSelectedOption(e.target.value)}>
+        <select  style={{padding: "8px",fontSize: "16px",border: "2px solid #ccc",borderRadius: "7px",}} 
+        id="select" value={selectedOption.toString()} onChange={(e) => setSelectedOption(e.target.value)}>
             <option value="false">false</option>
             <option value="true">true</option>
-        </select>
-        <button onClick={async function(){
+        </select><div></div>
+        <button  style={{padding: "8px 16px",fontSize: "16px",border: "2px solid #ccc",borderRadius: "7px",cursor: "pointer",}} 
+        onClick={async function(){
             await fetch("https://todo-backend-chy7.onrender.com/todo" ,{
                 method:"POST",
                 body:JSON.stringify({ // JSON will be captial
